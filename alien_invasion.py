18cb4dd5d060de05285420be8caac2883a90934d
TomekPk <src@vp.pl>
2017-10-06 13:16:28 +0200

    continue

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,7 +4,10 @@ import pygame # import module to make a game
 
 from settings import Settings
 from ship   import Ship
+from alien import Alien
+from game_stats import GameStats
 import game_functions as gf
+from pygame.sprite import Group
 
 
 
@@ -19,12 +22,26 @@ def run_game():
 
     # Make a ship
     ship = Ship(ai_settings, screen)
+    # Make a group to store bullets in.
+    bullets = Group()
+    # Make an alien
+    alien = Alien(ai_settings, screen)
+    aliens = Group()
+
+    # Create the fleet of aliens.
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    # Create an instance to store game statistics.
+    stats = GameStats(ai_settings)
 
     # Start the main loop for the game.
     while True:
-        gf.check_events(ship)
-        ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.check_events(ai_settings,screen,ship,bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets, aliens)
 
 
 
