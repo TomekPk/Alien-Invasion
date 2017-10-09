a2284b768bf8bd681bcad8254a87a9a8353396fd
TomekPk <src@vp.pl>
2017-10-09 10:03:23 +0200

    aliens speed increase

--- game_functions.py
+++ game_functions.py
@@ -38,6 +38,9 @@ def check_play_button(ai_settings, stats, screen, play_button, ship, aliens,
         create_fleet(ai_settings, screen, ship, aliens)
         ship.center_ship()
 
+        # Reset the game settings.
+        ai_settings.initialize_dynamic_settings()
+
         # Hide the mouse cursor.
         pygame.mouse.set_visible(False)
 
@@ -100,8 +103,9 @@ def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
     if len(aliens) == 0:
-        # Destroy existing bullets and create new fleet.
+        # Destroy existing bullets, speed up game, and create new fleet.
         bullets.empty()
+        ai_settings.increase_speed()
         create_fleet(ai_settings, screen, ship, aliens)
 
 
--- settings.py
+++ settings.py
@@ -22,7 +22,32 @@ class Settings():
 
         # Alien settings
         self.alien_speed_factor = 1
-        self.fleet_drop_speed = 100
+        self.fleet_drop_speed = 15
+
+        # How quickly the game speeds up
+        self.speed_up_scale = 1.5
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """ Initialize settings that change throughout the game."""
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 1
+
         # fleet_direction of 1 represents right; -1 represents left
         self.fleet_direction = 1
+
+    def increase_speed(self):
+        """ Increase speed settings."""
+        self.ship_speed_factor *= self.speed_up_scale
+        self.bullet_speed_factor *= self.speed_up_scale
+        self.alien_speed_factor *= self.speed_up_scale
+
+
+
+
+
+
+
 
